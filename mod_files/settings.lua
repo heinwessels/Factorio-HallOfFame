5b7a45006258f7ed9139081c41a73075e1fa67f6
Hein Wessels <heinwessels93@gmail.com>
2021-01-25 18:38:07 +0100

    Added alt-mode. Decreased playtime to 2 minutes. Removed stupid things.

---
+++ mod_files/settings.lua
@@ -0,0 +1,22 @@
+data:extend({
+    {    
+        type = "int-setting",
+        name = "hall-of-fame-playback-time",           
+        setting_type = "startup",        
+        default_value = 120,
+        minimum_value = 1,
+        maximum_value = 10000
+    },
+    {
+        type = "bool-setting",
+        name = "hall-of-fame-disable-defaults",        
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "hall-of-fame-camera-alt-info",        
+        setting_type = "startup",
+        default_value = false
+    }
+})
